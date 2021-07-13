7d9f414b2df993f88fee1ee83d7a6461350e2c7c
Lukas <lszwedxx@gmail.com>
2021-07-13 20:57:08 +0200

    Basics icons views

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("working")
+const icon = document.querySelectorAll(".list__item");
+const lotus = document.querySelector(".wrapper__btn");
+
+
+    lotus.addEventListener("click",e=>{
+        icon.forEach(item=>{
+            const dataClass = item.dataset.sound;
+            item.classList.toggle(dataClass)
+        })
+    })
